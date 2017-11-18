0388a0a4c66b007f0a50c26545495266dd48b99a
Ankur Kumar <30711979+ankurKumar1988@users.noreply.github.com>
2017-11-18 00:36:34 -0500

    Add files via upload

--- ConvenientConverter/ext_scripts.js
+++ ConvenientConverter/ext_scripts.js
@@ -47,6 +47,29 @@ function generateBoxes(){
 	for (k=0; k < dimensionArray.length; k++){
 		dimName = dimensionArray[k][0];
 		dimArray = dimensionArray[k][1];
+		// add listunit
+		appendData ='<div class="row">'+
+					'<div class="col-sm-4"></div>'+
+					'<div class="col-sm-4 unit">'+
+						'<div class="cellInputLabel">'+
+							'<input type="number" step="any" class="cellInput" id="selectinput1" onkeypress="convertUserSelect(event,'+k+',1)" onblur="convertUserSelect(event,'+k+',1)"><br>'+
+							'<select class="form-control cellLabel" style="height:28px;padding:2px;" id="selectLabel1"></select>'+
+						'</div>'+
+						'<div class="cellEqual">=<br>&nbsp</div>'+
+						'<div class="cellInputLabel">'+
+							'<input type="number" step="any" class="cellInput" id="selectinput2" onkeypress="convertUserSelect(event,'+k+',2)" onblur="convertUserSelect(event,'+k+',2)"><br>'+
+							'<select class="form-control cellLabel" style="height:28px;;padding:2px;" id="selectLabel2"></select>'+
+						'</div>'+
+						'<button  type="button" class="btn btn-warning uploadButton" onclick="uploadToSheetUserSelect('+k+')">'+
+							'<span class="glyphicon glyphicon-arrow-up"></span>'+
+						'</button>'+
+					'</div>'+
+					'<div class="col-sm-4"></div>'+
+					'</div>';
+		$(appendData).appendTo('#'+dimName+' #userSelect');		
+		$('<hr style="margin-top: 7px; border-color: #333;">').appendTo('#'+dimName+' #userSelect');
+		generateUserSelectList(k);
+		// add default boxes
 		for (i=0; i < dimArray.length; i++){
 			rowAppended = $('<div class="row" id="row'+(i+1)+'"></div>').appendTo('#'+dimName+' #boxes');
 			for (j=0; j < dimArray[i].length; j++){
@@ -69,7 +92,6 @@ function generateBoxes(){
 			}
 		}
 		$('<hr style="margin-top: 7px; border-color: #333;">').appendTo('#'+dimName);
-		
 		// Add custom field input from User
 		appendData = '<input class="userLabel" type="text" id="userlabel1" placeholder="Enter dimension 1 name">'+
 		             ' = <input type="number" id="usermultiplier" placeholder="Enter multiplier" style="text-align: center;">'+
@@ -131,7 +153,7 @@ function uploadToSheet(dimension,row,column){
 							'<input type="number" step="any" class="cellInput" id="input2" onkeypress="convert(event,-1,1,'+(nlist_items+1)+',2)" onblur="convert(event,-1,1,'+(nlist_items+1)+',2)"><br>'+
 							'<label style="display:inline-block;" class="cellLabel">'+unitArray[1]+'</label>'+
 						'</div>'+
-						'<button  type="button" class="btn btn-danger cancelButton" onclick="deleteFromSheet('+(i+1)+')">'+
+						'<button  type="button" class="btn btn-danger cancelButton" onclick="deleteFromSheet('+(nlist_items+1)+')">'+
 							'<span class="glyphicon glyphicon-remove"></span>'+
 						'</button>'+
 					'</div>'+
@@ -140,6 +162,86 @@ function uploadToSheet(dimension,row,column){
 	$(appendData).appendTo('#sortable');
 	$("#sheet"+(nlist_items+1)+" #input1").val(val1);
 	$("#sheet"+(nlist_items+1)+" #input2").val(val2);
+}
+
+function uploadToSheetUserSelect(dimension){
+	dimName = dimensionArray[dimension][0];
+	dimArray = dimensionArray[dimension][1];
+	var selectLabel1 = $('#'+dimName+' #selectLabel1').val();
+	var selectLabel2 = $('#'+dimName+' #selectLabel2').val();
+	// check if both units have been selected
+	if (selectLabel1 == null || selectLabel2 == null){alert('Please select both units');return;}
+	// check if equal labels
+	if (selectLabel1 == selectLabel2){alert('Please select different units');return;}
+	// find number of li items
+	var list_items = $('#sortable').children();
+	nlist_items = list_items.length;
+	// append new list item
+	var val1 = $('#'+dimName+' #selectinput1').val();
+	var val2 = $('#'+dimName+' #selectinput2').val();
+	var appendData = '<li id="sheet'+(nlist_items+1)+'">'+ 
+					'<div class="unit" style="display:inline-block;">'+
+						'<div class="cellInputLabel">'+
+							'<input type="number" step = "any" class="cellInput" id="input1" onkeypress="convert(event,-1,1,'+(nlist_items+1)+',1)" onblur="convert(event,-1,1,'+(nlist_items+1)+',1)"><br>'+
+							'<label style="display:inline-block;" class="cellLabel">'+selectLabel1+'</label>'+
+						'</div>'+
+						'<div class="cellEqual">=<br> &nbsp </div>'+
+						'<div class="cellInputLabel">'+
+							'<input type="number" step="any" class="cellInput" id="input2" onkeypress="convert(event,-1,1,'+(nlist_items+1)+',2)" onblur="convert(event,-1,1,'+(nlist_items+1)+',2)"><br>'+
+							'<label style="display:inline-block;" class="cellLabel">'+selectLabel2+'</label>'+
+						'</div>'+
+						'<button  type="button" class="btn btn-danger cancelButton" onclick="deleteFromSheet('+(nlist_items+1)+')">'+
+							'<span class="glyphicon glyphicon-remove"></span>'+
+						'</button>'+
+					'</div>'+
+				'</li>'
+	
+	// generate the conversion array
+	var multiplier;
+	var constant;
+	var found = 0;
+	
+	// check if the two labels are already there together
+	for (i=0; i < dimArray.length; i++){
+		for (j=0; j < dimArray[i].length; j++){
+			if (dimArray[i][j][0]==selectLabel1){
+				if (dimArray[i][j][1]==selectLabel2){multiplier=dimArray[i][j][2];constant=dimArray[i][j][3];sheetArray.push([selectLabel1,selectLabel2,multiplier,constant]);found=1;}
+			}
+			if (dimArray[i][j][1]==selectLabel1){
+				if (dimArray[i][j][0]==selectLabel2){multiplier=1/dimArray[i][j][2];constant=-dimArray[i][j][3]/dimArray[i][j][2];sheetArray.push([selectLabel1,selectLabel2,multiplier,constant]);found=1;}
+			}
+			if (dimArray[i][j][0]==selectLabel2){
+				if (dimArray[i][j][1]==selectLabel1){multiplier=1/dimArray[i][j][2];constant=-dimArray[i][j][3]/dimArray[i][j][2];sheetArray.push([selectLabel1,selectLabel2,multiplier,constant]);found=1;}
+			}
+			if (dimArray[i][j][1]==selectLabel2){
+				if (dimArray[i][j][0]==selectLabel1){multiplier=dimArray[i][j][2];constant=dimArray[i][j][3];sheetArray.push([selectLabel1,selectLabel2,multiplier,constant]);found=1;}
+			}
+		}
+	}
+	
+	// find labels via depth-first search
+	if (found == 0){
+		labelVisited = [];
+		var outputVal = treeSearchforSheetUpload(selectLabel1,1,2,selectLabel2);
+		if (outputVal.length >= 1){
+			var outputVal1 = outputVal[0];
+			var outputVal2 = outputVal[1];
+			multiplier = outputVal2-outputVal1;
+			constant = 2*outputVal1-outputVal2;
+			sheetArray.push([selectLabel1,selectLabel2,multiplier,constant]);
+			found = 1;
+		}
+		else{
+			alert('Chosen unit conversion relationship not available. Upload failed.');
+		}
+	}
+	
+	// check if new box need to be added
+	if (found == 1){
+		$(appendData).appendTo('#sortable');
+		$("#sheet"+(nlist_items+1)+" #input1").val(val1);
+		$("#sheet"+(nlist_items+1)+" #input2").val(val2);
+	}
 }
 
 function deleteFromSheet(liPos){
@@ -180,6 +282,9 @@ function addToList(dimension){
 								'<input type="number" step="any" class="cellInput" id="input2" onkeypress="convert(event,'+dimension+','+row+','+col+',2)" onblur="convert(event,'+dimension+','+row+','+col+',2)"><br>'+
 								'<label class="cellLabel">'+userlabel2+'</label>'+
 							'</div>'+
+							'<button  type="button" class="btn btn-warning uploadButton" onclick="uploadToSheet('+dimension+','+row+','+col+')">'+
+								'<span class="glyphicon glyphicon-arrow-up"></span>'+
+							'</button>'+
 						'</div>';
 		// decide if new row class is needed
 		if (col == 1) {
@@ -191,6 +296,8 @@ function addToList(dimension){
 		}
 		// save new dimensionArray to local storage
 		localStorage.setItem("dimensionArray", JSON.stringify(dimensionArray));
+		// update userSelect list
+		generateUserSelectList(dimension);
 	}
 }
 
@@ -239,4 +346,195 @@ function convert(event,dimension,row,column,posInput){
 		}
 	}
 	event.stopPropagation();
+}
+
+function convertUserSelect(event,dimension,posInput){
+	if (event.type == 'blur' || event.which == 13 || event.keyCode == 13) {
+		dimName = dimensionArray[dimension][0];
+		dimArray = dimensionArray[dimension][1];
+		// get ids
+		var inputID = "#"+dimName+" "+"#selectinput"+posInput;
+		var inputlabel = $("#"+dimName+" "+"#selectLabel"+posInput).val();
+		if ($(inputID).val().length < 1){event.stopPropagation();return;}
+		var posOutput = posInput == 1 ? 2 : 1;
+		var outputID = "#"+dimName+" "+"#selectinput"+posOutput;
+		var outputlabel = $("#"+dimName+" "+"#selectLabel"+posOutput).val();
+		
+		// check if equal labels
+		var selectLabel1 = $('#'+dimName+' #selectLabel1').val();
+		var selectLabel2 = $('#'+dimName+' #selectLabel2').val();
+		if (selectLabel1 == selectLabel2){$(outputID).val($(inputID).val());event.stopPropagation();return;}
+		// check if both units have been selected
+		if (selectLabel1 == null || selectLabel2 == null){event.stopPropagation();return;}
+		
+		// check if the two labels are already there together
+		for (i=0; i < dimArray.length; i++){
+			for (j=0; j < dimArray[i].length; j++){
+				if (dimArray[i][j][0]==selectLabel1){
+					if (dimArray[i][j][1]==selectLabel2){userSelectOutput(inputID,outputID,'direct',posInput,dimArray[i][j],event,inputlabel,outputlabel);event.stopPropagation();return;}
+				}
+				if (dimArray[i][j][1]==selectLabel1){
+					if (dimArray[i][j][0]==selectLabel2){userSelectOutput(inputID,outputID,'reverse',posInput,dimArray[i][j],event,inputlabel,outputlabel);event.stopPropagation();return;}
+				}
+				if (dimArray[i][j][0]==selectLabel2){
+					if (dimArray[i][j][1]==selectLabel1){userSelectOutput(inputID,outputID,'reverse',posInput,dimArray[i][j],event,inputlabel,outputlabel);event.stopPropagation();return;}
+				}
+				if (dimArray[i][j][1]==selectLabel2){
+					if (dimArray[i][j][0]==selectLabel1){userSelectOutput(inputID,outputID,'direct',posInput,dimArray[i][j],event,inputlabel,outputlabel);event.stopPropagation();return;}
+				}
+			}
+		}
+		
+		// find labels via depth-first search
+		labelVisited = [];
+		var outputVal = treeSearch(inputlabel,$(inputID).val(),outputlabel);
+		if (outputVal != ''){
+			$(outputID).val(parseFloat(outputVal.toFixed(3)));
+			// Put conversion on the right when converted via 'enter'
+			if (event.which == 13 || event.keyCode == 13) {
+				var appendData = '<li>'+$(inputID).val()+' '+inputlabel+' = '+parseFloat(outputVal.toFixed(3))+' '+outputlabel+'</li>';
+				$(appendData).appendTo('#recentList');
+				$('#recentConversions').css('display','block');
+			}	
+		}
+	}
+	event.stopPropagation();
+}
+
+function treeSearchforSheetUpload(inputlabel,inputVal1,inputVal2,outputlabel){
+	var returnedOutput; var returnedOutput1; var returnedOutput2;
+	labelVisited.push(inputlabel);
+	for (var i=0; i < dimArray.length; i++){
+		for (var j=0; j < dimArray[i].length; j++){
+			if (dimArray[i][j][0]==inputlabel){
+				if (dimArray[i][j][1]==outputlabel){
+					returnedOutput1 = inputVal1*dimArray[i][j][2]+dimArray[i][j][3];
+					returnedOutput2 = inputVal2*dimArray[i][j][2]+dimArray[i][j][3];
+					return [returnedOutput1,returnedOutput2];
+				}
+				else{
+					if (labelVisited.indexOf(dimArray[i][j][1])>-1){
+						continue;
+					}
+					var newinputVal1 = inputVal1*dimArray[i][j][2]+dimArray[i][j][3];
+					var newinputVal2 = inputVal2*dimArray[i][j][2]+dimArray[i][j][3];
+					returnedOutput = treeSearchforSheetUpload(dimArray[i][j][1],newinputVal1,newinputVal2,outputlabel);
+					if (returnedOutput.length >= 1){
+						return returnedOutput;
+					}
+				}
+				continue;
+			}
+			if (dimArray[i][j][1]==inputlabel){
+				if (dimArray[i][j][0]==outputlabel){
+					returnedOutput1 = (inputVal1-dimArray[i][j][3])/dimArray[i][j][2];
+					returnedOutput2 = (inputVal2-dimArray[i][j][3])/dimArray[i][j][2];
+					return [returnedOutput1,returnedOutput2];
+				}
+				else{
+					if (labelVisited.indexOf(dimArray[i][j][0])>-1){
+						continue;
+					}
+					var newinputVal1 = (inputVal1-dimArray[i][j][3])/dimArray[i][j][2];
+					var newinputVal2 = (inputVal2-dimArray[i][j][3])/dimArray[i][j][2];
+					returnedOutput = treeSearchforSheetUpload(dimArray[i][j][0],newinputVal1,newinputVal2,outputlabel);
+					if (returnedOutput.length >= 1){
+						return returnedOutput;
+					}
+				}
+			}
+		}
+	}
+	returnedOutput =[];
+	return returnedOutput;
+}
+
+function treeSearch(inputlabel,inputVal,outputlabel){
+	var returnedOutput;
+	labelVisited.push(inputlabel);
+	for (var i=0; i < dimArray.length; i++){
+		for (var j=0; j < dimArray[i].length; j++){
+			if (dimArray[i][j][0]==inputlabel){
+				if (dimArray[i][j][1]==outputlabel){
+					returnedOutput = inputVal*dimArray[i][j][2]+dimArray[i][j][3];
+					return returnedOutput;
+				}
+				else{
+					if (labelVisited.indexOf(dimArray[i][j][1])>-1){
+						continue;
+					}
+					var newinputVal = inputVal*dimArray[i][j][2]+dimArray[i][j][3];
+					returnedOutput = treeSearch(dimArray[i][j][1],newinputVal,outputlabel);
+					if (returnedOutput != ''){
+						return returnedOutput;
+					}
+				}
+				continue;
+			}
+			if (dimArray[i][j][1]==inputlabel){
+				if (dimArray[i][j][0]==outputlabel){
+					returnedOutput = (inputVal-dimArray[i][j][3])/dimArray[i][j][2];
+					return returnedOutput;
+				}
+				else{
+					if (labelVisited.indexOf(dimArray[i][j][0])>-1){
+						continue;
+					}
+					var newinputVal = (inputVal-dimArray[i][j][3])/dimArray[i][j][2];
+					returnedOutput = treeSearch(dimArray[i][j][0],newinputVal,outputlabel);
+					if (returnedOutput != ''){
+						return returnedOutput;
+					}
+				}
+			}
+		}
+	}
+	returnedOutput ='';
+	return returnedOutput;
+}
+
+function userSelectOutput(inputID,outputID,direction,posInput,unitArray,event,inputlabel,outputlabel){
+	var input = $(inputID).val();
+	var multiplier = unitArray[2];
+	var constant = unitArray[3];
+	if (direction == 'direct'){
+		var  output = posInput == 1 ? input*multiplier+constant: (input-constant)/multiplier;
+	}
+	else {
+		var  output = posInput == 2 ? input*multiplier+constant: (input-constant)/multiplier;
+	}
+	$(outputID).val(parseFloat(output.toFixed(3)));
+	// Put conversion on the right when converted via 'enter'
+	if (event.which == 13 || event.keyCode == 13) {
+		var appendData = '<li>'+input+' '+inputlabel+' = '+parseFloat(output.toFixed(3))+' '+outputlabel+'</li>';
+		$(appendData).appendTo('#recentList');
+		$('#recentConversions').css('display','block');
+	}
+}
+
+function generateUserSelectList(k){
+	var uniqueUnits = [];
+	dimName = dimensionArray[k][0];
+	dimArray = dimensionArray[k][1];
+	// empty current list
+	$('#'+dimName+' #selectLabel1').empty();
+	$('#'+dimName+' #selectLabel2').empty();
+	// find all unique units
+	for (i=0; i < dimArray.length; i++){
+		for (j=0; j < dimArray[i].length; j++){
+			if (uniqueUnits.indexOf(dimArray[i][j][0])==-1){
+				uniqueUnits.push(dimArray[i][j][0]);
+			}
+			if (uniqueUnits.indexOf(dimArray[i][j][1])==-1){
+				uniqueUnits.push(dimArray[i][j][1]);
+			}
+		}
+	}
+	// put all unique units in selectable label
+	$('<option disabled selected>Choose unit</option>').appendTo('#'+dimName+' #selectLabel1');
+	$('<option disabled selected>Choose unit</option>').appendTo('#'+dimName+' #selectLabel2');
+	for (l=0; l < uniqueUnits.length; l++){
+		$('<option>'+uniqueUnits[l]+'</option>').appendTo('#'+dimName+' #selectLabel1');
+		$('<option>'+uniqueUnits[l]+'</option>').appendTo('#'+dimName+' #selectLabel2');
+	}
 }
--- ConvenientConverter/initial_boxes.js
+++ ConvenientConverter/initial_boxes.js
@@ -1,13 +1,23 @@
-var dimensionArray = localStorage.getItem("dimensionArray");
+var dimensionArray;
+//dimensionArray = localStorage.getItem("dimensionArray");
 if (dimensionArray != undefined && dimensionArray != null) {
 	dimensionArray = JSON.parse(dimensionArray);
 }
 else {
-	var lengthArray = [[["meter(s)","cm(s)",100,0],["meter(s)","inches(s)",39.3701,0],["meter(s)","ft(s)",3.28084,0]],
-				   [["meter(s)","cm(s)",100,0],["meter(s)","inches(s)",39.3701,0],["meter(s)","ft(s)",3.28084,0]]];
-	var massArray = [[["kg","g",1000,0],["kg","pound",2.20462,0],["kg","ounce",35.274,0]]];
-	dimensionArray = [['length',lengthArray],['mass',massArray]];
+	var ASUArray = [[["cubic ft (NTP)","scf (60F, 14.696 psia)",0.98112,0],["cubic ft (NTP)","g.mol",1.1727,0],["Btu","J",1054.4,0]],
+					[["Btu","cal(g)",252,0]]];
+	var lengthArray = [[["cm(s)","inch(es)",0.3937,0],["meter(s)","inch(es)",39.3701,0],["meter(s)","ft(s)",3.28084,0]],
+				   [["mile(s)","km(s)",1.6093,0],["meter(s)","yard(s)",1.0936,0],["Nautical mile(s)","km(s)",1.852,0]],
+				   [["Nautical mile(s)","mile(s)",1.1507,0],["mile(s)","ft(s)",5280,0]]];
+	var massArray = [[["kg","pound",2.20462,0],["kg","ounce",35.274,0],["metric ton","kg",1000,0]],
+					 [["ounce","g",28.3495,0],["pound","g",453.5929,0]]];
+	var pressureArray = [[["atm","bar",1.01325,0],["atm","Pa",101325,0],["atm","psi",14.6959,0]],
+					 [["atm","atm(g)",1,-1],["KPa","psi",0.145,0],["KPa(g)","psi(g)",0.145,0]]];
+	var customArray = [];				 
+	dimensionArray = [['ASU',ASUArray],['length',lengthArray],['mass',massArray],['pressure',pressureArray],['custom',customArray]];
 }
+
+localStorage.setItem("dimensionArray", JSON.stringify(dimensionArray));
 
 var sheetArray = localStorage.getItem("sheetArray");
 if (sheetArray != undefined && sheetArray != null) {
